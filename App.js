b02fea1cb10185b6fc91c5affb27edbe882c9f25
emba2ra3star <tachi.i.061@gmail.com>
2024-03-04 13:48:10 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textColor}>數位二甲</Text>
+      <Text style={styles.textColor}>111119023 陳嘉馨</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textColor: {
+    color: 'blue',
+  },
+});
